fc189b232c55fbd0b613d784ad49dc271935ca65
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-28 16:40:46 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            title: "Mailing List",
+            emails: []
+        }
+    },
+    methods:{
+        
+    }
+}).mount("#app");
